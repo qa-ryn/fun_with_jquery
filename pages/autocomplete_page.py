3fe5d19f98a7b59bb1bd0ae0f789f25c40f86eea
ryn2 <ihategreengoblin@gmail.com>
2025-09-30 14:02:07 +0700

    wip on auto_complete.py

--- pages/autocomplete_page.py
+++ pages/autocomplete_page.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import Page
 
 class AutoComplete:
-    words = ["C", "P", "Ja", "R"]
+    words = ["C", "P", "Ja", "Jo"]
     def __init__(self, page: Page):
         self.page = page
         
@@ -9,23 +9,44 @@ class AutoComplete:
         self.page.goto(url)
         
     def _get_iframe(self):
-        """Return the iframe's content frame."""
         return self.page.locator("iframe").content_frame
         
     def _handle_input(self, iframe, word):
-        input_locator = iframe.locator("#tags")
+        tag_locator = iframe.locator("#tags")
+        developer_locator = iframe.locator("#developer")
+        if tag_locator.is_visible():
+            input_locator = iframe.locator("#tags")
+            
+            input_locator.fill("")
+            input_locator.type(word)
+            input_locator.press("Enter")
+            
+            items = iframe.locator("ul.ui-autocomplete li")
+            self.page.wait_for_timeout(2000)
+            suggestions = items.all_inner_texts()
+            for suggestion in suggestions:
+                print(f"Input {word} -> Suggestions = {suggestion}")
+        elif developer_locator.is_visible():
+            input_locator = iframe.locator("#developer")
+            
+            input_locator.fill("")
+            input_locator.type(word)
+            input_locator.press("Enter")
+            
+            items = iframe.locator("ul.ui-autocomplete li")
+            self.page.wait_for_timeout(2000)
+            suggestions = items.all_inner_texts()
+            for suggestion in suggestions:
+                print(f"Input {word} -> Suggestions = {suggestion}")
+        else:
+            print("Code reach here")
         
-        input_locator.fill("")
-        input_locator.type(word)
-        input_locator.press("Enter")
-        
-        items = iframe.locator("ul.ui-autocomplete li")
-        self.page.wait_for_timeout(2000)
-        suggestions = items.all_inner_texts()
-        for suggestion in suggestions:
-            print(f"Input {word} -> Suggestions = {suggestion}")
-
     def default_functionality(self):
+        iframe = self._get_iframe()
+        for word in self.words:
+            self._handle_input(iframe, word)
+            
+    def accent_folding(self):
         iframe = self._get_iframe()
         for word in self.words:
             self._handle_input(iframe, word)
--- tests/test_autoComplete.py
+++ tests/test_autoComplete.py
@@ -5,3 +5,8 @@ def test_default_functionality(page: Page):
     run = AutoComplete(page)
     run.load_page("https://jqueryui.com/autocomplete/#default")
     run.default_functionality()
+    
+def test_accent_folding(page: Page):
+    run = AutoComplete(page)
+    run.load_page("https://jqueryui.com/autocomplete/#folding")
+    run.accent_folding()
